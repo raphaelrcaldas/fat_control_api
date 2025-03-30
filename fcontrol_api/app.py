ff109cd23e3944339b339a227cc4ec2cdd1a5cd6
raphael r caldas <raphael008@hotmail.com>
2025-03-29 22:24:36 -0300

    ajuste app.py cors

--- fcontrol_api/app.py
+++ fcontrol_api/app.py
@@ -1,6 +1,3 @@
-import subprocess
-from contextlib import asynccontextmanager
-
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
@@ -8,21 +5,10 @@ from fcontrol_api.routers import auth, indisp, ops, postos, users
 
 origins = [
     'http://localhost:3000',
+    'https://fatcontrol.vercel.app'
 ]
 
-
-@asynccontextmanager
-async def run_migrations(app: FastAPI):
-    """Executa as migrações do Alembic antes da inicialização da aplicação."""
-    try:
-        subprocess.run(['alembic', 'upgrade', 'head'], check=True)
-        print('Migrações do Alembic aplicadas com sucesso!')
-    except subprocess.CalledProcessError as e:
-        print(f'Erro ao aplicar migrações: {e}')
-    yield
-
-
-app = FastAPI(lifespan=run_migrations)
+app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
