b40a12fb021b873683820f4288b4f641daa216aa
raphael r caldas <raphael008@hotmail.com>
2025-05-14 15:04:21 -0300

    ajuste em diarias

--- fcontrol_api/models/cegep/diarias.py
+++ fcontrol_api/models/cegep/diarias.py
@@ -29,8 +29,8 @@ class DiariaValor(Base):
     __tablename__ = 'valor_diarias'
 
     id: Mapped[int] = mapped_column(Identity(), init=False, primary_key=True)
-    grupo_pg: Mapped[int] = mapped_column(ForeignKey(GrupoPg.id))
-    grupo_cid: Mapped[int] = mapped_column(ForeignKey(GrupoCidade.id))
+    grupo_pg: Mapped[int]
+    grupo_cid: Mapped[int]
     valor: Mapped[float] = mapped_column(nullable=False)
     data_inicio: Mapped[date] = mapped_column(nullable=False)
     data_fim: Mapped[date] = mapped_column(nullable=True)
