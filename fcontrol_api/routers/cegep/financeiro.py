a3764c622a59b3431fd20988be6b5519fcccf9b1
raphael r caldas <raphael008@hotmail.com>
2025-07-30 09:10:35 -0300

    ajuste no calculo de custo de missoes

--- fcontrol_api/routers/cegep/financeiro.py
+++ fcontrol_api/routers/cegep/financeiro.py
@@ -7,15 +7,11 @@ from sqlalchemy.ext.asyncio import AsyncSession
 
 from fcontrol_api.database import get_session
 from fcontrol_api.models.cegep.diarias import GrupoCidade, GrupoPg
-from fcontrol_api.models.cegep.missoes import FragMis, PernoiteFrag, UserFrag
+from fcontrol_api.models.cegep.missoes import FragMis, UserFrag
 from fcontrol_api.models.public.users import User
 from fcontrol_api.schemas.missoes import FragMisSchema, UserFragMis
 from fcontrol_api.services.financeiro import cache_diarias, cache_soldos
-from fcontrol_api.utils.datas import listar_datas_entre
-from fcontrol_api.utils.financeiro import (
-    buscar_soldo_por_dia,
-    buscar_valor_por_dia,
-)
+from fcontrol_api.utils.financeiro import custo_missao
 
 Session = Annotated[AsyncSession, Depends(get_session)]
 
@@ -66,8 +62,7 @@ async def get_pgto(
         stmt = stmt.where(FragMis.regres <= fim)
 
     result = await session.execute(stmt)
-
-    user_mis: list[tuple[UserFrag, FragMis]] = result.all()
+    result: list[tuple[UserFrag, FragMis]] = result.all()
 
     # Cache de valores de diária por grupo
     valores_cache = await cache_diarias(session)
@@ -88,71 +83,26 @@ async def get_pgto(
     )
 
     response = []
-    for usr_frg, missao in user_mis:
-        grupo_pg = grupos_pg.get(usr_frg.p_g)
-        usr = UserFragMis.model_validate(usr_frg).model_dump(
+    for usr_frg, missao in result:
+        usr_frg = UserFragMis.model_validate(usr_frg).model_dump(
             exclude={'user_id', 'frag_id'}
         )
         mis = FragMisSchema.model_validate(missao).model_dump(
-            exclude={'users', 'id'}
+            exclude={'users'}
+        )
+        mis = custo_missao(
+            usr_frg['p_g'],
+            usr_frg['sit'],
+            mis,
+            grupos_pg,
+            grupos_cidade,
+            valores_cache,
+            soldos_cache,
         )
 
-        item = {
-            **usr,
-            **mis,
-            'gp_pg': grupo_pg,
-            'dias': 0,
-            'diarias': 0,
-            'valor_total': 0,
-            'qtd_acres_desloc': 0,
-        }
-
-        pernoites: list[PernoiteFrag] = item['pernoites']
-        for pnt in pernoites:
-            grupo_cidade = grupos_cidade.get(pnt['cidade_id'], 3)
-            pnt['gp_cid'] = grupo_cidade
-            pnt['diarias'] = 0
-            pnt['subtotal'] = 0
-
-            dias = listar_datas_entre(pnt['data_ini'], pnt['data_fim'])
-            for dia in dias[:-1]:
-                valor_dia: float
-                if item['sit'] == 'g':
-                    valor_soldo = buscar_soldo_por_dia(
-                        item['p_g'], dia, soldos_cache
-                    )
-                    valor_dia = valor_soldo * 0.02  # 2% do soldo
-                else:
-                    valor_dia = buscar_valor_por_dia(
-                        grupo_pg, grupo_cidade, dia, valores_cache
-                    )
-                    pnt['diarias'] += 1
-
-                pnt['subtotal'] += valor_dia
-
-            if item['sit'] == 'g':
-                continue
-
-            if pnt['meia_diaria']:
-                valor_ultimo = buscar_valor_por_dia(
-                    grupo_pg,
-                    grupo_cidade,
-                    pnt['data_fim'].date(),
-                    valores_cache,
-                )
-                pnt['diarias'] += 0.5
-                pnt['subtotal'] += valor_ultimo * 0.5
-
-            if pnt['acrec_desloc']:
-                pnt['subtotal'] += 95
-                item['qtd_acres_desloc'] += 1
-
-        item['dias'] = len(listar_datas_entre(item['afast'], item['regres']))
-        item['diarias'] = sum(list(map(lambda x: x['diarias'], pernoites)))
-        item['valor_total'] = sum(
-            list(map(lambda x: x['subtotal'], pernoites))
-        )
-
-        response.append(item)
+        response.append({
+            'user_mis': usr_frg,
+            'missao': mis,
+        })
 
     return response
--- fcontrol_api/utils/financeiro.py
+++ fcontrol_api/utils/financeiro.py
@@ -1,22 +1,8 @@
 from datetime import date
 
 from fcontrol_api.models.cegep.diarias import DiariaValor
-from fcontrol_api.models.cegep.missoes import PernoiteFrag
 from fcontrol_api.models.public.posto_grad import Soldo
-
-
-def qtd_dias_pnt(pernoite: PernoiteFrag) -> float:
-    if not pernoite.data_ini or not pernoite.data_fim:
-        return 0.0
-
-    data_ini = pernoite.data_ini.date()
-    data_fim = pernoite.data_fim.date()
-    dias = max((data_fim - data_ini).days, 0)
-
-    if pernoite.meia_diaria:
-        dias += 0.5
-
-    return dias
+from fcontrol_api.utils.datas import listar_datas_entre
 
 
 def buscar_valor_por_dia(
@@ -41,3 +27,104 @@ def buscar_soldo_por_dia(pg: str, data: date, cache: dict) -> float:
         ):
             return item.valor
     return 0.0
+
+
+def custo_pernoite(
+    pg,
+    sit,
+    ini,
+    fim,
+    gp_pg,
+    gp_cid,
+    meia_diaria,
+    ac_desloc,
+    soldos_cache,
+    vals_cache,
+):
+    custo = {'subtotal': 0, 'qtd_ac': 0, 'vals': [], 'dias': 0}
+    val_ag: dict = {}
+
+    dias_validos = listar_datas_entre(ini, fim)
+    for dia in dias_validos[:-1]:
+        valor_dia: float
+        if sit == 'g':
+            valor_soldo = buscar_soldo_por_dia(pg, dia, soldos_cache)
+            valor_dia = valor_soldo * 0.02  # 2% do soldo
+        else:
+            valor_dia = buscar_valor_por_dia(gp_pg, gp_cid, dia, vals_cache)
+
+        if valor_dia not in val_ag:
+            val_ag[valor_dia] = {'valor': valor_dia, 'qtd': 0}
+
+        val_ag[valor_dia]['qtd'] += 1
+        custo['subtotal'] += valor_dia
+        custo['dias'] += 1
+
+    if sit != 'g':
+        if meia_diaria:
+            valor_ultimo = buscar_valor_por_dia(
+                gp_pg, gp_cid, dias_validos[-1], vals_cache
+            )
+            custo['dias'] += 1
+            custo['subtotal'] += valor_ultimo * 0.5
+            val_ag[valor_ultimo]['qtd'] += 0.5
+
+        if ac_desloc:
+            custo['ac_desloc'] = 95
+            custo['subtotal'] += custo['ac_desloc']
+
+    custo['vals'] = list(val_ag.values())
+
+    return custo
+
+
+def custo_missao(
+    p_g: str,
+    sit: str,
+    mis: dict,
+    grupos_pg: dict,
+    grupos_cidade: dict,
+    valores_cache: dict,
+    soldos_cache: dict = None,
+) -> dict:
+    """
+    Modifica o dicionário da missão adicionando os custos calculados
+    com base nos pernoites.
+    """
+    grupo_pg = grupos_pg.get(p_g)
+
+    mis['dias'] = 0
+    mis['diarias'] = 0
+    mis['valor_total'] = 0
+    mis['qtd_ac'] = 0
+
+    for pnt in mis['pernoites']:
+        grupo_cidade = grupos_cidade.get(pnt['cidade']['codigo'], 3)
+        pnt['gp_cid'] = grupo_cidade
+
+        custo = custo_pernoite(
+            p_g,
+            sit,
+            pnt['data_ini'],
+            pnt['data_fim'],
+            grupo_pg,
+            grupo_cidade,
+            pnt['meia_diaria'],
+            pnt['acrec_desloc'],
+            soldos_cache,
+            valores_cache,
+        )
+
+        pnt['custo'] = custo
+
+        if pnt['acrec_desloc']:
+            mis['qtd_ac'] += 1
+
+        if sit != 'g':
+            for val in custo['vals']:
+                mis['diarias'] += val['qtd']
+
+        mis['valor_total'] += custo['subtotal']
+        mis['dias'] += pnt['custo']['dias']
+
+    return mis
