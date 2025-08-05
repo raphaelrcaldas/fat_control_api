6f3948fcdb3f4e7d31a7a63f7cf7ef31fe4d6985
Raphael R Caldas <raphael008@hotmail.com>
2025-08-05 15:25:58 -0300

    - alterando parametro da sessão do banco expire on commit true

--- fcontrol_api/database.py
+++ fcontrol_api/database.py
@@ -2,9 +2,9 @@ from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 
 from fcontrol_api.settings import Settings
 
-engine = create_async_engine(Settings().DATABASE_URL)  # type: ignore
+engine = create_async_engine(Settings().DATABASE_URL)
 
 
 async def get_session():
-    async with AsyncSession(engine, expire_on_commit=False) as session:
+    async with AsyncSession(engine, expire_on_commit=True) as session:
         yield session
